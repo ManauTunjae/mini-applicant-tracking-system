39ae6016c3b8e71b5773cb51190e241a82aa6240
ManauTunjae <manau.tunjae@hotmail.com>
2026-03-18 19:54:24 +0100

    feat: create ApplyJob components

---
+++ src/components/ApplyJob.jsx
@@ -0,0 +1,11 @@
+import { React } from "react";
+import { useParams, Link } from "react-router-dom";
+import { supabase } from "../supabaseClient";
+
+return (
+    <div>
+
+    </div>
+);
+
+export default ApplyJob;
