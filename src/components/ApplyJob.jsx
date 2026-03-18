82aebe9dac5cc68dbfc5d852970b4503980da837
ManauTunjae <manau.tunjae@hotmail.com>
2026-03-18 20:07:04 +0100

    feat: fetch job details

--- src/components/ApplyJob.jsx
+++ src/components/ApplyJob.jsx
@@ -2,10 +2,29 @@ import { React } from "react";
 import { useParams, Link } from "react-router-dom";
 import { supabase } from "../supabaseClient";
 
-return (
-    <div>
+const ApplyJob = () => {
+    const { jobId } = useParams();
+    const [ job, setjob ] = useState(null);
 
-    </div>
-);
+    const [ name, setName ] = useState("");
+    const [ email, setEmail ] = useState("");
+    const [ linkedin, setLinkedin ] = useState("");
+
+   useEffect(() => {
+    const fetchJobDetails = async () => {
+        const { data, error } = await supabase
+        .from("jobs")
+        .select("title, company")
+        .eg("id", jobId)
+        .single();
+
+        if (!error) setjob(data);
+        setLoading(false);
+    };
+    fetchJobDetails();
+   }, [jobId]); 
+
+  return <div></div>;
+};
 
 export default ApplyJob;
